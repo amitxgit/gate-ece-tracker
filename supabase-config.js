4ef96bf4bab91dc29d6b6f00199941fd13e62994
Amit Kumar <amitkrcontact@gmail.com>
2026-07-09 02:08:54 +0530

    Add Supabase configuration file

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+window.SUPABASE_CONFIG = {
+  url: "https://frycqobijwklsmwhdlff.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZyeWNxb2JpandrbHNtd2hkbGZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM1Mjk2NDIsImV4cCI6MjA5OTEwNTY0Mn0.Kaf2LTGDVh99JoDHXjGsChrSauA1eH0BQPVdYq20oz8"
+};
